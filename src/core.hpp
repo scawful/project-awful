9457ab5f4f1e7883258c044f63823a85c68966d5
Justin Scofield <justin.scofield@outlook.com>
2021-05-12 11:50:44 -0400

    added core header file

---
+++ src/core.hpp
@@ -0,0 +1,13 @@
+// Core Project Header
+// 
+// Contains the includes for all major libraries being used in the project
+
+#ifndef core_hpp
+#define core_hpp
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#endif core_hpp
