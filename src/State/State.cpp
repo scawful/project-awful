803fe16567f5d1df3ccd4a62ba2939791602deb0
Justin Scofield <justin.scofield@outlook.com>
2021-05-13 11:25:13 -0400

    created virtual state class, uses std::stack to handle game states

---
+++ src/State/State.cpp
@@ -0,0 +1,62 @@
+#include "State.hpp"
+
+State::State(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states) 
+{
+    this->window = window;
+    this->supportedKeys = supportedKeys;
+    this->states = states;
+    this->quit = false;
+    this->paused = false;
+    this->keytime = 0.f;
+    this->keytimeMax = 10.f;
+}
+
+State::~State() 
+{
+    
+}
+
+// Accessors
+const bool & State::getQuit() const 
+{
+    return this->quit;
+}
+
+const bool State::getKeytime()
+{
+    if ( this->keytime >= this->keytimeMax ) 
+    {
+        this->keytime = 0.f;
+        return true;
+    }
+    return false;
+}
+
+// Functions 
+void State::endState() 
+{
+    this->quit = true;
+}
+
+void State::pauseState() 
+{
+    this->paused = true;
+}
+void State::unpauseState() 
+{
+    this->paused = false;
+}
+
+void State::updateMousePositions() 
+{
+    this->mousePosScreen = sf::Mouse::getPosition();
+    this->mousePosWindow = sf::Mouse::getPosition(*this->window);
+    this->mousePosView = this->window->mapPixelToCoords(sf::Mouse::getPosition(*this->window));
+}
+
+void State::updateKeytime(const float& dt)
+{
+    if (this->keytime < this->keytimeMax)
+        this->keytime += 100.f * dt;
+}
+
---
+++ src/State/State.hpp
@@ -0,0 +1,49 @@
+#ifndef State_hpp
+#define State_hpp
+
+#include "../core.hpp"
+
+class State 
+{
+private:
+    
+protected:
+    // Variables
+    std::stack<State*>* states;
+    sf::RenderWindow* window;
+    std::map<std::string, int>* supportedKeys;
+    std::map<std::string, int> keybinds;
+    bool quit;
+    bool paused;
+    float keytime;
+    float keytimeMax;
+    
+    sf::Vector2i mousePosScreen;
+    sf::Vector2i mousePosWindow;
+    sf::Vector2f mousePosView;
+        
+    // Resources
+    std::map<std::string, sf::Texture> textures;
+        
+public:
+    State(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states);
+    virtual ~State();
+    
+    // Accessors
+    const bool& getQuit() const;
+    const bool getKeytime();
+    
+    // Functions
+    void endState();
+    void pauseState();
+    void unpauseState();
+    
+    // True Virtual Boys
+    virtual void updateMousePositions();
+    virtual void updateKeytime(const float& dt);
+    virtual void updateInput(const float& dt) = 0;
+    virtual void update(const float& dt) = 0;
+    virtual void render(sf::RenderTarget* target = nullptr) = 0;
+};
+
+#endif /* State_hpp */
