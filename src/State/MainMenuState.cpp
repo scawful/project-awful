85ffe26bdfd9ed441251e1944ba2c81844875547
Justin Scofield <justin.scofield@outlook.com>
2021-05-13 11:29:48 -0400

    created MainMenuState, implements parent class State's functions

---
+++ src/State/MainMenuState.cpp
@@ -0,0 +1,83 @@
+#include "MainMenuState.hpp"
+
+// Initializer functions
+void MainMenuState::initVariables() 
+{
+    
+}
+
+void MainMenuState::initBackground() 
+{
+    this->background.setSize(
+         sf::Vector2f
+         (
+              static_cast<float>(this->window->getSize().x),
+              static_cast<float>(this->window->getSize().y)
+         )
+    );
+    
+    // if (!this->backgroundTexture.loadFromFile("background.jpg")) 
+    // {
+    //     throw "ERROR::MAINMENUSTATE::FAILED_TO_LOAD_BACKGROUND_TEXTURE";
+    // }
+    
+    //this->background.setTexture(&this->backgroundTexture);
+}
+
+void MainMenuState::initFonts() 
+{    
+    if (!this->menu_font.loadFromFile("../assets/ACARDECLASSIC.ttf")) {
+        std::cout << "Error couldn't load menu font" << "\n";
+    }       
+}
+
+MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states)
+    : State(window, supportedKeys, states)
+{
+    this->initVariables();
+    this->initBackground();
+    this->initFonts();
+}
+
+MainMenuState::~MainMenuState()
+{
+
+}
+
+void MainMenuState::updateInput(const float & dt) 
+{
+
+}
+
+void MainMenuState::update(const float& dt) 
+{
+    this->updateMousePositions();
+    this->updateInput(dt); 
+}
+
+void MainMenuState::render(sf::RenderTarget* target)
+{
+    if (!target)
+        target = this->window;
+    
+    target->draw(this->background);
+        
+    // Literally the title lmao
+    sf::Text title;
+    title.setString("Project Awful");
+    title.setFillColor(sf::Color::Black);
+    title.setFont(this->menu_font);
+    title.setPosition(470, 30);
+    title.setCharacterSize(60);
+    target->draw(title);
+    
+    // Positional coordinates mouse tracing
+    sf::Text mouseText;
+    mouseText.setPosition(this->mousePosView.x,this->mousePosView.y - 20);
+    mouseText.setFont(this->menu_font);
+    mouseText.setCharacterSize(12);
+    stringstream ss;
+    ss << this->mousePosView.x << " " << this->mousePosView.y;
+    mouseText.setString(ss.str());
+    target->draw(mouseText);
+}
---
+++ src/State/MainMenuState.hpp
@@ -0,0 +1,31 @@
+#ifndef MainMenuState_hpp
+#define MainMenuState_hpp
+
+#include "State.hpp"
+
+class MainMenuState : public State 
+{
+private:
+    // Variables
+    sf::Texture backgroundTexture;
+    sf::RectangleShape background;
+    sf::Font menu_font;
+                
+    // Functions
+    void initVariables();
+    void initBackground();
+    void initFonts();
+    
+public:
+    MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys, std::stack<State*>* states);
+    virtual ~MainMenuState();
+        
+    // Functions
+    void getQuit();
+    void updateInput(const float& dt);
+    void updateButtons();
+    void update(const float& dt);
+    void render(sf::RenderTarget* target = nullptr);
+};
+
+#endif /* MainMenuState_hpp */
