88e3d5c2fef7f7e0b6d0dec393476a5528139083
Justin Scofield <justin.scofield@outlook.com>
2021-08-25 23:10:22 -0400

    Started implementing the Textbox and Button solutions employed in MainMenuState in the CharacterState, currently doesn't work cause of the event pointer I think, still need to investigate.

--- src/State/CharacterState.hpp
+++ src/State/CharacterState.hpp
@@ -6,14 +6,26 @@
 class CharacterState : public State
 {
 private:
-    void initFonts();
-    void initTextures();
+    shared_ptr<sf::Event> sfEvent;
     sf::Font characterFont;
     sf::Text textboxOutput;
 
+    std::map<std::string, Button*> buttons;
+    std::map<std::string, Textbox*> textboxes;
+
+    void initFonts();
+    void initTextures();
+    void initButtons();
+    void initTextboxes();
+
 public:
-    CharacterState(sf::RenderWindow* window, std::stack<State*>* states);
+    CharacterState(sf::RenderWindow* window, std::stack<State*>* states, shared_ptr<sf::Event> event);
     virtual ~CharacterState();
+
+    void updateInput(const float& dt);
+    void updateButtons();
+    void updateTextboxes();
+    void neutralizeTextboxes();
 
     // Functions    
     void update(const float& dt);
