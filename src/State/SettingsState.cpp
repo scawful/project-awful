fd2b8ff024cf99be3a4b2bccff916d1d1fa126ff
Justin Scofield <justin.scofield@outlook.com>
2022-01-03 19:40:36 -0500

    More comments wow so organized and cool

--- src/State/SettingsState.cpp
+++ src/State/SettingsState.cpp
@@ -6,6 +6,11 @@ void SettingsState::initVariables()
     
 }
 
+/**
+ * @brief Initialize the background size 
+ * @author @scawful
+ * 
+ */
 void SettingsState::initBackground() 
 {
     this->background.setSize(
@@ -17,6 +22,11 @@ void SettingsState::initBackground()
     );
 }
 
+/**
+ * @brief Load any fonts to be used in the SettingsState
+ * @author @scawful
+ * 
+ */
 void SettingsState::initFonts() 
 {    
     if ( !this->menu_font.loadFromFile("../assets/ARCADECLASSIC.TTF") ) 
@@ -26,6 +36,11 @@ void SettingsState::initFonts()
         
 }
 
+/**
+ * @brief Initialize keybinds set by the user from a file 
+ * @author @scawful
+ * 
+ */
 void SettingsState::initKeybinds()
 {
     std::fstream ifs("gamestate_binds.ini");
@@ -42,6 +57,13 @@ void SettingsState::initKeybinds()
     ifs.close();
 }
 
+/**
+ * @brief Load any Graphical objects to be used in the SettingsState
+ * @author @scawful
+ * 
+ * @todo Separate this into multiple initialization functions (Buttons, DropDownLists, etc)
+ * 
+ */
 void SettingsState::initGui() 
 {
 
@@ -57,6 +79,13 @@ void SettingsState::initGui()
     this->dropDownLists["RESOLUTION"] = new DropDown(sf::Vector2f((SCREEN_WIDTH - 250.f) / 2, 150), sf::Vector2f(250.f, 75.f), menu_font, li, 3);
 }
 
+/**
+ * @brief Construct a new Settings State:: Settings State object
+ * @author @scawful
+ * 
+ * @param window 
+ * @param states 
+ */
 SettingsState::SettingsState(sf::RenderWindow* window, std::stack<State*>* states)
     : State(window, states)
 {
@@ -67,6 +96,11 @@ SettingsState::SettingsState(sf::RenderWindow* window, std::stack<State*>* state
     this->initGui();
 }
 
+/**
+ * @brief Destroy the Settings State:: Settings State object
+ * @author @scawful
+ * 
+ */
 SettingsState::~SettingsState()
 {
     auto it = this->buttons.begin();
@@ -88,6 +122,14 @@ void SettingsState::updateInput(const float & dt)
 
 }
 
+/**
+ * @brief Update GUI objects 
+ * @author scawful
+ * 
+ * @todo Separate this into multiple functions 
+ * 
+ * @param dt 
+ */
 void SettingsState::updateGui(const float& dt)
 {
     // Update all GUI elements in state and handles functionalty
@@ -109,6 +151,12 @@ void SettingsState::updateGui(const float& dt)
     
 }
 
+/**
+ * @brief Cumulative update routine for the SettingsState
+ * @author @scawful
+ * 
+ * @param dt 
+ */
 void SettingsState::update(const float& dt)
 {
     this->updateMousePositions();
@@ -117,6 +165,14 @@ void SettingsState::update(const float& dt)
     this->updateGui(dt);
 }
 
+/**
+ * @brief Render any GUI Objects to the screen
+ * @author @scawful
+ * 
+ * @todo separate function bing bong 
+ * 
+ * @param target 
+ */
 void SettingsState::renderGui(sf::RenderTarget& target)
 {
     for (auto &it : this->buttons) 
@@ -130,6 +186,14 @@ void SettingsState::renderGui(sf::RenderTarget& target)
     }
 }
 
+/**
+ * @brief Cumulative render function for the SettingsState 
+ * @author @scawful
+ * 
+ * @todo make the mouse text its own function (renderMouse)
+ * 
+ * @param target 
+ */
 void SettingsState::render(sf::RenderTarget* target)
 {
     if (!target)
--- src/State/SettingsState.hpp
+++ src/State/SettingsState.hpp
@@ -6,16 +6,21 @@
 class SettingsState : public State
 {
 private:
-    // Variables
+    // sfml Texture Object 
     sf::Texture backgroundTexture;
+
+    // sfml RectangleShape Object 
     sf::RectangleShape background;
+
+    // sfml Font Object 
     sf::Font menu_font;
         
+    // Custom Object Data Structures 
     std::map<std::string, Button*> buttons;
     std::map<std::string, DropDown*> dropDownLists;
     std::map<std::string, int>* supportedKeys;
             
-    // Functions
+    // Initialization functions 
     void initVariables();
     void initBackground();
     void initFonts();
@@ -26,11 +31,15 @@ public:
     SettingsState(sf::RenderWindow* window, std::stack<State*>* states);
     virtual ~SettingsState();
     
-    // Functions
+    // Accessors
     void getQuit();
+
+    // Update routines 
     void updateInput(const float& dt);
     void updateGui(const float& dt);
     void update(const float& dt);
+
+    // Render routines 
     void renderGui(sf::RenderTarget& target);
     void render(sf::RenderTarget* target = nullptr);
 };
